e6c1ebd596aaa8c5a429b861de275c754ca5739a
Brian Retterer <bretterer@gmail.com>
2026-01-04 17:34:42 -0500

    Fix prepare referral payout day

--- routes/console.php
+++ routes/console.php
@@ -20,7 +20,7 @@ Schedule::job(new \App\Jobs\SendCampaignChatReminders)->dailyAt('08:00');
 
 Schedule::job(new \App\Jobs\CalculateReferralPayouts)->monthlyOn(1, '06:00');
 Schedule::job(new \App\Jobs\MonitorTemporaryReferralPayouts)->dailyAt('00:01');
-Schedule::job(new \App\Jobs\PrepareReferralPayouts)->monthlyOn(15, '06:00');
+Schedule::job(new \App\Jobs\PrepareReferralPayouts)->monthlyOn(1, '06:00');
 Schedule::job(new \App\Jobs\ProcessReferralPayouts(attemptNumber: 1))->monthlyOn(15, '10:00');
 Schedule::job(new \App\Jobs\ProcessReferralPayouts(attemptNumber: 2))->monthlyOn(18, '10:00');
 Schedule::job(new \App\Jobs\ProcessReferralPayouts(attemptNumber: 3))->monthlyOn(21, '10:00');
